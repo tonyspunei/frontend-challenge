5c5b5685b990e0146dd4393984b4112bade9a6db
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 14:42:58 +0100

    render Footer content

---
+++ src/lib/components/ui/Footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import ConstrainedWidth from '$lib/components/layout/ConstrainedWidth.svelte';
+  import Icon from '@iconify/svelte'
+</script>
+
+<ConstrainedWidth as="footer" class="py-12">
+  <div class="flex items-center justify-between">
+    <span>© 2021 Flowbite, LLC. All rights reserved.</span>
+  
+    <ul class="flex md:space-x-6">
+      <li>
+        <a href="https://www.facebook.com">
+          <Icon icon="fa:facebook" class="w-6 h-6 text-[#9CA3AF]" />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/home">
+          <Icon icon="fa:twitter" class="w-6 h-6 text-[#9CA3AF]" />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/">
+          <Icon icon="fa:github" class="w-6 h-6 text-[#9CA3AF]" />
+        </a>
+      </li>
+      <li>
+        <a href="https://dribbble.com/">
+          <Icon icon="fa:dribbble" class="w-6 h-6 text-[#9CA3AF]" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</ConstrainedWidth>
