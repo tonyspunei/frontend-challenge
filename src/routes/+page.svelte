7520016239eb950f0ac31679450c8d72cacfc794
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 14:43:39 +0100

    replace static content & render Hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
-<section class="h-full w-full flex flex-col items-center justify-center gap-10">
-	<h1 class="text-4xl font-bold max-w-4xl px-8">
-		Welcome to your <span class="bg-[#00fe37] px-1">personal Challenge</span> @THE ARC!
-	</h1>
-	<p class="max-w-4xl px-8 text-center">
-		Wir sind froh, dass du dich der Herausforderung stellst und freuen uns auf deine Ergebnisse.
-		<br />
-		Falls du Fragen zur Challenge hast, kannst du dich jederzeit an uns wenden.
-		<br />
-		Dein Startpunkt ist die <code class="bg-dark text-white p-1 rounded">README.md</code>
-	</p>
-</section>
+<script lang="ts">
+	import Hero from "$lib/components/ui/Hero.svelte";
+
+	export let content = {
+		Hero: {
+			heading: "We invest in the world's potential.",
+			blurb: "Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth."
+		}
+	};
+</script>
+
+<Hero content={content.Hero} />
