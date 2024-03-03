ba8e2243de2bef608518881d3f1d04f102806479
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 19:45:50 +0100

    improve Hero mobile design

--- src/lib/components/ui/Hero.svelte
+++ src/lib/components/ui/Hero.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <ConstrainedWidth class="pt-24 overflow-hidden">
-  <div class="flex items-center">
-    <div class="flex flex-col items-center w-8/12 text-center space-y-8 py-20">
+  <div class="flex flex-col md:flex-row items-center">
+    <div class="flex flex-col items-center md:w-8/12 text-center space-y-8 py-2 md:py-20">
       <div class="space-y-6">
         {#if content?.heading}
           <h1 class="text-6xl text-gray-900">{content.heading}</h1>
@@ -17,7 +17,7 @@
         {/if}
       </div>
 
-      <div class="flex items-center space-x-6">
+      <div class="flex items-center space-x-4 md:space-x-6">
         <a class="bg-blue-600 text-white py-3 px-6 rounded-lg flex items-center space-x-3" href="/learn-more">
           <span>Learn more</span>
           <Icon icon="fa:arrow-right" />
@@ -29,8 +29,8 @@
       </div>
     </div>
 
-    <div class="flex w-4/12 pb-[37.5vw] relative">
-      <picture class="flex w-[50vw] absolute aspect-[785/630]">
+    <div class="flex md:w-4/12 md:pb-[37.5vw] relative">
+      <picture class="flex md:w-[50vw] md:absolute aspect-square md:aspect-[785/630]">
         <img class="relative h-full w-full object-cover object-top" src="/images/hero-img.png" alt="">
       </picture>
     </div>
