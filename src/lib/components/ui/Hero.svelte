b9eba64af4024e2f7ccf312575a4ffc437cf0d59
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 14:43:06 +0100

    render Hero

---
+++ src/lib/components/ui/Hero.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import ConstrainedWidth from '$lib/components/layout/ConstrainedWidth.svelte';
+	import Icon from '@iconify/svelte';
+
+  export let content;
+</script>
+
+<ConstrainedWidth class="pt-24">
+  <div class="flex items-center">
+    <div class="flex flex-col items-center flex-1 text-center space-y-8 py-20">
+      <div class="space-y-6">
+        {#if content?.heading}
+          <h1 class="text-6xl text-gray-900">{content.heading}</h1>
+        {/if}
+        {#if content?.blurb}
+          <p class="text-gray-500 text-xl">{content.blurb}</p>
+        {/if}
+      </div>
+
+      <div class="flex items-center space-x-6">
+        <a class="bg-blue-600 text-white py-3 px-6 rounded-lg flex items-center space-x-3" href="/learn-more">
+          <span>Learn more</span>
+          <Icon icon="fa:arrow-right" />
+        </a>
+        <a class="border border-gray-300 text-gray-800 py-3 px-6 rounded-lg flex items-center space-x-3" href="https://www.youtube.com">
+          <Icon icon="fa:video-camera" />
+          <span>Watch Video</span>
+        </a>
+      </div>
+    </div>
+
+    <div class="flex-1 pb-[37.5vw] relative">
+      <picture class="flex w-[50vw] absolute aspect-[785/630]">
+        <img class="relative h-full w-full object-cover" src="/images/hero-img.png" alt="">
+      </picture>
+    </div>
+  </div>
+</ConstrainedWidth>
