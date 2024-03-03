dd4eaf361b5aa176e72676ad2cb8072e17bd9ba5
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 14:41:17 +0100

    add import alias in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import UnoCSS from 'unocss/vite'
+import path from 'path';
 
 export default defineConfig({
 	plugins: [UnoCSS(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	resolve: {
+    alias: {
+      $: path.resolve(__dirname, './src')
+    }
+  }
 });
