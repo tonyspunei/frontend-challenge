69a55cec6cba524670f1232ae1ca55d6e6053020
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 14:41:38 +0100

    add ConstrainedWidth general component

---
+++ src/lib/components/layout/ConstrainedWidth.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let as: string = 'section';
+  export let backgroundColor: string = 'bg-white';
+</script>
+
+<svelte:element
+  this={as}
+  class={`relative w-full ${$$props.class} ${backgroundColor}`}
+>
+  <div class="max-w-screen-xl mx-auto px-6 md:px-8">
+    <slot />
+  </div>
+</svelte:element>
