83050baf5f714c4a97697db60f373b79393976a9
Tony Spunei <tony.spunei@yahoo.com>
2024-03-03 19:51:30 +0100

    improve Gallery mobile design

--- src/lib/components/blocks/Gallery.svelte
+++ src/lib/components/blocks/Gallery.svelte
@@ -2,7 +2,7 @@
   import ConstrainedWidth from "$lib/components/layout/ConstrainedWidth.svelte";
 </script>
 
-<ConstrainedWidth class="py-24">
+<ConstrainedWidth class="py-12 md:py-24">
   <div class="flex flex-col items-center text-center space-y-12">
     <div class="space-y-4">
       <h2 class="text-4xl text-gray-800">We didn't reinvent the wheel</h2>
@@ -10,22 +10,22 @@
     </div>
 
     <div class="grid grid-cols-4 items-center gap-4">
-      <picture class="col-span-2 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-2 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-1.jpg" alt="team">
       </picture>
-      <picture class="col-span-1 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-1 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-2.jpg" alt="team">
       </picture>
-      <picture class="col-span-1 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-1 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-3.jpg" alt="team">
       </picture>
-      <picture class="col-span-1 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-1 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-4.jpg" alt="team">
       </picture>
-      <picture class="col-span-2 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-2 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-5.jpg" alt="team">
       </picture>
-      <picture class="col-span-1 h-full w-full rounded-lg overflow-hidden">
+      <picture class="col-span-1 h-60 md:h-full w-full rounded-lg overflow-hidden">
         <img class="w-full h-full object-cover" src="/images/image-6.jpg" alt="team">
       </picture>
     </div>
