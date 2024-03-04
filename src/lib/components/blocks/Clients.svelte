38f0972f90b89392966d663cfdc5a03c53c681e1
Tony Spunei <tony.spunei@yahoo.com>
2024-03-04 06:24:13 +0100

    render images & heading dynamically

--- src/lib/components/blocks/Clients.svelte
+++ src/lib/components/blocks/Clients.svelte
@@ -1,26 +1,32 @@
 <script lang="ts">
 	import ConstrainedWidth from "$lib/components/layout/ConstrainedWidth.svelte";
 
+  interface Content {
+    heading: string;
+    blurb: string;
+    images: { src: string; alt: string; }[];
+  }
 
+  export let content: Content;
 </script>
 
 <ConstrainedWidth class="py-12 md:py-24">
   <div class="flex flex-col items-center space-y-12">
-    <h2 class="text-4xl">You’ll be in good company</h2>
+    {#if content?.heading}
+      <h2 class="text-4xl">{content.heading}</h2>
+    {/if}
+    {#if content?.blurb}
+      <p>{content.blurb}</p>
+    {/if}
   
-    <ul class="flex items-center justify-center flex-wrap gap-14">
-      <li>
-        <img src="/images/Google.png" alt="google logo">
-      </li>
-      <li>
-        <img src="/images/Microsoft.png" alt="microsoft logo">
-      </li>
-      <li>
-        <img src="/images/Spotify.png" alt="spotify logo">
-      </li>
-      <li>
-        <img src="/images/Mailchimp.png" alt="mailchimp logo">
-      </li>
-    </ul>
+    {#if content?.images?.length}
+      <ul class="flex items-center justify-center flex-wrap gap-14">
+        {#each content.images as image}
+          <li>
+            <img src={image.src} alt={image.alt}>
+          </li>
+        {/each}
+      </ul>
+    {/if}
   </div>
 </ConstrainedWidth>
